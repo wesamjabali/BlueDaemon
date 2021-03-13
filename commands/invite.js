777b4d43841b0a2f56265d666da193a2fab04758
Wesam Jabali <wesamjabali@icloud.com>
2021-03-13 04:29:29 -0600

    add invite and invitebot commands

---
+++ commands/invite.js
@@ -0,0 +1,17 @@
+const config = require("../config.json");
+module.exports = {
+  name: "invite",
+  description: "Create a server invite",
+  privileged: false,
+  usage: config.prefix + "invite",
+  execute(msg, isModerator, client) {
+    msg.channel
+      .createInvite({
+        maxAge: 0,
+        unique: true,
+      })
+      .then((inv) => {
+        msg.channel.send(`Request sent to your DM, ${msg.author}`);
+        msg.author.send(`Here's your invite: ${inv}`);      });
+  },
+};
---
+++ commands/inviteBot.js
@@ -0,0 +1,26 @@
+const config = require("../config.json");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "invitebot",
+  description: "Invite me to your server",
+  privileged: false,
+  usage: config.prefix + "inviteBot",
+  execute(msg, isModerator, client) {
+    const inviteEmbed = new Discord.MessageEmbed()
+      .setTitle("BlueDaemon")
+      .setURL(
+        "https://discord.com/api/oauth2/authorize?client_id=819103115693260860&permissions=0&scope=bot"
+      )
+      .addField(
+        "Thanks for checking me out!",
+        "Follow the link above to add me to your server"
+      )
+      .setColor(config.primaryColor)
+      .setTimestamp()
+      .setImage(config.banner);
+
+    msg.channel.send(`Response sent to your DM, ${msg.author}`);
+    msg.author.send(inviteEmbed);
+  },
+};
