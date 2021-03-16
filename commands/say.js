67e06e5b0131e07e45d8dc5c0daa8febc7a88937
Wesam Jabali <wesamjabali@icloud.com>
2021-03-16 14:42:57 -0500

    split on command size

--- commands/say.js
+++ commands/say.js
@@ -5,9 +5,8 @@ module.exports = {
   privileged: true,
   usage: "say <message>",
   execute: async (msg, isModerator, isFaculty, client) => {
-    await msg.channel.send(
-      msg.content.substring(4)
-    );
+    const say = msg.content.substring(module.exports.name.length + 1);
+    await msg.channel.send(say);
     msg.delete();
   },
 };
