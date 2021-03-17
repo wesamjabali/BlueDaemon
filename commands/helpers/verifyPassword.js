2281f694a90344f7f602233f7f8f5b8e6880ee9c
Wesam Jabali <wesamjabali@icloud.com>
2021-03-16 22:27:06 -0500

    DChange where clause for password verification

--- commands/helpers/verifyPassword.js
+++ commands/helpers/verifyPassword.js
@@ -5,7 +5,7 @@ module.exports =
   // Verify a password is correct
   async function verifyPassword(roleID, password) {
     const [protectedRole] = await knex("cdm_role_password")
-      .where({ role_id: roleID, guild_id: guildID })
+      .where({ role_id: roleID })
       .select("password");
     return bcrypt.compareSync(password, protectedRole.password);
   };
