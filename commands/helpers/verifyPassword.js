f808b52d5cf421e91bac720b7c8a317d3a1b6640
Wesam Jabali <wesamjabali@icloud.com>
2021-03-16 22:28:18 -0500

    wrap protectedRole

--- commands/helpers/verifyPassword.js
+++ commands/helpers/verifyPassword.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 module.exports =
   // Verify a password is correct
   async function verifyPassword(roleID, password) {
-    const [protectedRole] = await knex("cdm_role_password")
+    const protectedRole = await knex("cdm_role_password")
       .where({ role_id: roleID })
       .select("password");
     return bcrypt.compareSync(password, protectedRole.password);
