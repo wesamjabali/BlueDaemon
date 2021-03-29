b25cd77e5c118bb0ba36ed82a77d4c502c122254
Wesam Jabali <wesamjabali@icloud.com>
2021-03-29 15:13:57 -0500

    remove bcrypt

--- commands/helpers/updateQuarter.js
+++ commands/helpers/updateQuarter.js
@@ -1,7 +1,6 @@
 const knex = require("../../knex");
 
 module.exports = async function protectRole(guildID, newQuarter) {
-  let bcryptPass = bcrypt.hashSync(password, salt);
   await knex("cdm_guild_config")
     .where({ guild_id: guildID })
     .update({ current_quarter: newQuarter });
