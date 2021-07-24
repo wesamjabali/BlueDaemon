998090c903444b4502f0ae19f39e2a6f82603e78
Wesam Jabali <wesamjabali@icloud.com>
2021-07-24 12:13:38 -0500

    switch source to gitlab

--- commands/source.js
+++ commands/source.js
@@ -2,15 +2,15 @@ const config = require("../config.json");
 const Discord = require("discord.js");
 module.exports = {
   name: "source",
-  description: "View my source code on GitHub",
+  description: "View my source code on GitLab",
   facultyOnly: false,
   privileged: false,
   usage: "source",
   execute: async (msg, isModerator, isFaculty, client) => {
     const sourceEmbed = new Discord.MessageEmbed()
-      .setTitle("View my source code on Github")
+      .setTitle("View my source code on GitLab")
       .setDescription("Author: Wesam Jabali")
-      .setURL("https://github.com/wesamjabali/BlueDaemon")
+      .setURL("https://gitlab.com/wesamjabali/BlueDaemon")
       .addField(
         "\u200B",
         "Like this bot? Consider [buying me a coffee!](https://www.buymeacoffee.com/wesamjabali)"
